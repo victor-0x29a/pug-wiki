3ddb88dddcc36af34a9ae3653347eee3f714c593
Victor <cartoonbr495@gmail.com>
2024-01-26 14:35:58 -0400

    Chore: updating the category controller after invertion of dependencies

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { CategoryService } from "../entities/category";
 import { ICategory } from '../entities/dto/category.dto';
+import { CategoryRepository } from '../entities/category/repository';
+
+const category = new CategoryService(new CategoryRepository())
 
 export const CategoryController = {
     async getAll(req: express.Request, res: express.Response, next: express.NextFunction) {
-        return CategoryService.findAll()
+        return category.findAll()
             .then((data: ICategory[]) => {
                 return res.status(200).render('category', {
                     categories: data
