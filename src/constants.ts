479b2c1ff1a3063c5f74a88f080458c6c77c55c8
victor-0x29a <cartoonbr495@gmail.com>
2024-01-26 21:33:13 -0400

    Chore: adding the navbarItems in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,15 @@
 export const isStaging = process.env.NODE_ENV === 'dev'
+export const itemsNavBar = [
+    {
+        label: "Categorias",
+        href: "/category"
+    },
+    {
+        label: "Ajude a manter",
+        href: "/help"
+    },
+    {
+        label: "Entrar",
+        href: "/auth/signin"
+    }
+]
--- src/middlewares/template.ts
+++ src/middlewares/template.ts
@@ -1,33 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import { itemsNavBar } from "../constants";
 
 export const TemplateMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    res.locals.itemsNavBar = [
-        {
-            label: "Categorias",
-            href: "/category"
-        },
-        {
-            label: "Ajude a manter",
-            href: "/auth/help"
-        },
-        {
-            label: "Entrar",
-            href: "/auth/signin"
-        }
-    ]
-    res.locals.dropdownItems = [
-        {
-            label: "Categorias",
-            href: "/category"
-        },
-        {
-            label: "Ajude a manter",
-            href: "/auth/help"
-        },
-        {
-            label: "Entrar",
-            href: "/auth/signin"
-        }
-    ]
+    res.locals.itemsNavBar = itemsNavBar
     next()
 }
