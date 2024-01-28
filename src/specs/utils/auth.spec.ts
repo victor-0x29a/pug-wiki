c358c41df128e178af3e273db99b03b9fdb66ec3
victor-0x29a <cartoonbr495@gmail.com>
2024-01-27 22:43:42 -0400

    Feat: specs to auth util with 100% of covered lines

---
+++ src/specs/utils/auth.spec.ts
@@ -0,0 +1,41 @@
+import { Auth } from "../../utils";
+
+const authInstance = new Auth()
+
+test('should generate a token with success', () => {
+    expect(typeof authInstance.generateToken(1)).toBe('string')
+})
+
+test('should verify a token with success', () => {
+    const token = authInstance.generateToken(1)
+    expect(authInstance.verifyToken(token)).toBe(true)
+})
+
+test('should verify a token with fail', () => {
+    const token = 'fake-token'
+    expect(authInstance.verifyToken(token)).toBe(false)
+})
+
+test('should decode a token with success', () => {
+    const PERMISSION_LEVEL = 1
+    const token = authInstance.generateToken(PERMISSION_LEVEL)
+    const { permissionLevel } = authInstance.decodeToken(token)
+    expect(permissionLevel).toBe(PERMISSION_LEVEL)
+})
+
+test('should reset an auth params with success', () => {
+    const AUTHORIZATION = "foo-bar"
+    const USERNAME = "fooBar"
+    let requestSimulated = {
+        session: {
+            authorization: AUTHORIZATION,
+            username: USERNAME
+        }
+    } as any
+
+    authInstance.cleanAuthParams(requestSimulated)
+
+    const { authorization, username } = requestSimulated.session
+    expect(authorization).toBe("")
+    expect(username).toBe("")
+})
