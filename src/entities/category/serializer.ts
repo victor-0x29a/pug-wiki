dca1174c492939c1f38eef8edf1eaf8b067ca011
victor-0x29a <cartoonbr495@gmail.com>
2024-01-27 23:00:19 -0400

    Chore: abstracting the schemas from category serializer

--- src/entities/category/serializer.ts
+++ src/entities/category/serializer.ts
@@ -9,7 +9,10 @@ const TYPE_ERROR_LABEL = 'Confira a label, ela é obrigatória e deve ser um tex
 const REQUIRED_SLUG = 'O slug é obrigatório.'
 const TYPE_ERROR_SLUG = 'Confira o slug, ele é obrigatório e deve ser um texto.'
 
+const labelSchema = string().max(20).required(REQUIRED_LABEL).typeError(TYPE_ERROR_LABEL)
+const slugSchema = string().max(30).required(REQUIRED_SLUG).typeError(TYPE_ERROR_SLUG)
+
 export const ICategorySchema = object().shape({
-    label: string().max(20).required(REQUIRED_LABEL).typeError(TYPE_ERROR_LABEL),
-    slug: string().max(30).required(REQUIRED_SLUG).typeError(TYPE_ERROR_SLUG)
+    label: labelSchema,
+    slug: slugSchema
 })
