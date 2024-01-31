38fa0683e0f500229a6ab2846d7a09273d615689
victor-0x29a <cartoonbr495@gmail.com>
2024-01-31 19:32:46 -0400

    Feat: bypass protect middleware added when is staging

--- src/middlewares/protect.ts
+++ src/middlewares/protect.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { Auth, ExpressUtils } from "../utils";
+import { isStaging } from "../constants";
 
 const LOGIN_ENDPOINT_REDIRECT = '/auth/signin'
 const UNAUTHORIZED_ENDPOINT_REDIRECT = '/backoffice/me'
@@ -7,6 +8,8 @@ const UNAUTHORIZED_ENDPOINT_REDIRECT = '/backoffice/me'
 const authInstance = new Auth()
 
 export const ProtectMiddleware = (permissionLevelRequired: 1 | 2) => (req: Request, res: Response, next: NextFunction) => {
+    if (isStaging) next()
+
     const token = req.session.authorization
     const hasToken = Boolean(token)
     if (!hasToken) {
