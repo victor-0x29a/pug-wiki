ba1c55f375e05eca3a5684424936fa5e543aa83a
victor-0x29a <cartoonbr495@gmail.com>
2024-02-01 07:16:17 -0400

    Chore: extending the class AppError with the class Error

--- src/appError.ts
+++ src/appError.ts
@@ -1,10 +1,11 @@
-class AppError {
+class AppError extends Error {
     public pugApplicationError: null | string = null
     public isRest = false
     public restMessage = ''
     public statusCode = 400
 
     constructor(error: string, isRest = false, restMessage = '', statusCode = 400) {
+        super(error)
         this.pugApplicationError = error
         this.isRest = isRest
         this.restMessage = restMessage
